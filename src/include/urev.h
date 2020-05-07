574c6083c148cff15ab2098952a7e0a6d8c4d11c
Hiroaki Nakamura <hnakamur@gmail.com>
2020-05-07 13:36:52 +0000

    Update api docs

--- src/include/urev.h
+++ src/include/urev.h
@@ -43,102 +43,102 @@ static inline void urev_queue_exit(urev_queue_t *queue)
 
 /**
  * completion handler type for a accept operation.
- * @param [in] op     a accept operation.
+ * @param [in,out] op     a accept operation.
  */
 typedef void (*urev_accept_handler_t)(urev_accept_op_t *op);
 
 /**
  * completion handler type for a connect operation.
- * @param [in] op     a connect operation.
+ * @param [in,out] op     a connect operation.
  */
 typedef void (*urev_connect_handler_t)(urev_connect_op_t *op);
 
 /**
  * completion handler type for a close operation.
- * @param [in] op     a close operation.
+ * @param [in,out] op     a close operation.
  */
 typedef void (*urev_close_handler_t)(urev_close_op_t *op);
 
 /**
  * completion handler type for a fadvise operation.
- * @param [in] op     a fadvise operation.
+ * @param [in,out] op     a fadvise operation.
  */
 typedef void (*urev_fadvise_handler_t)(urev_fadvise_op_t *op);
 
 /**
  * completion handler type for a fallocate operation.
- * @param [in] op     a fallocate operation.
+ * @param [in,out] op     a fallocate operation.
  */
 typedef void (*urev_fallocate_handler_t)(urev_fallocate_op_t *op);
 
 /**
  * completion handler type for a fsync operation.
- * @param [in] op     a fsync operation.
+ * @param [in,out] op     a fsync operation.
  */
 typedef void (*urev_fsync_handler_t)(urev_fsync_op_t *op);
 
 /**
  * completion handler type for a madvise operation.
- * @param [in] op     a madvise operation.
+ * @param [in,out] op     a madvise operation.
  */
 typedef void (*urev_madvise_handler_t)(urev_madvise_op_t *op);
 
 /**
  * completion handler type for a openat operation.
- * @param [in] op     a openat operation.
+ * @param [in,out] op     a openat operation.
  */
 typedef void (*urev_openat_handler_t)(urev_openat_op_t *op);
 
 /**
  * completion handler type for a openat2 operation.
- * @param [in] op     a openat2 operation.
+ * @param [in,out] op     a openat2 operation.
  */
 typedef void (*urev_openat2_handler_t)(urev_openat2_op_t *op);
 
 /**
  * completion handler type for a read or write operation.
- * @param [in] op     a read or write operation.
+ * @param [in,out] op     a read or write operation.
  */
 typedef void (*urev_read_or_write_handler_t)(urev_read_or_write_op_t *op);
 
 /**
  * completion handler type for a readv or writev operation.
- * @param [in] op     a readv or writev operation.
+ * @param [in,out] op     a readv or writev operation.
  */
 typedef void (*urev_readv_or_writev_handler_t)(urev_readv_or_writev_op_t *op);
 
 /**
  * completion handler type for a recv or send operation.
- * @param [in] op     a recv or send operation.
+ * @param [in,out] op     a recv or send operation.
  */
 typedef void (*urev_recv_or_send_handler_t)(urev_recv_or_send_op_t *op);
 
 /**
  * completion handler type for a recvmsg or sendmsg operation.
- * @param [in] op     a recvmsg or sendmsg operation.
+ * @param [in,out] op     a recvmsg or sendmsg operation.
  */
 typedef void (*urev_recvmsg_or_sendmsg_handler_t)(urev_recvmsg_or_sendmsg_op_t *op);
 
 /**
  * completion handler type for a splice operation.
- * @param [in] op     a splice operation.
+ * @param [in,out] op     a splice operation.
  */
 typedef void (*urev_splice_handler_t)(urev_splice_op_t *op);
 
 /**
  * completion handler type for a statx operation.
- * @param [in] op     a statx operation.
+ * @param [in,out] op     a statx operation.
  */
 typedef void (*urev_statx_handler_t)(urev_statx_op_t *op);
 
 /**
  * completion handler type for a timeout operation.
- * @param [in] op     a timeout operation.
+ * @param [in,out] op     a timeout operation.
  */
 typedef void (*urev_timeout_handler_t)(urev_timeout_op_t *op);
 /**
  * completion handler type for a timeout_remove operation.
- * @param [in] op     a timeout_remove operation.
+ * @param [in,out] op     a timeout_remove operation.
  */
 typedef void (*urev_timeout_cancel_handler_t)(urev_timeout_cancel_op_t *op);
 
@@ -157,7 +157,7 @@ struct urev_accept_op {
     int              fd;
     struct sockaddr *addr;
     socklen_t       *addrlen;
-    int             flags;
+    int              flags;
 };
 
 struct urev_connect_op {
@@ -267,11 +267,11 @@ struct urev_recv_or_send_op {
     urev_op_common_t            common; // must be the first field
     urev_recv_or_send_handler_t handler;
 
-    int       sockfd;
-    void     *buf;
-    size_t    len;
-    int       flags;
-    off_t     offset;
+    int     sockfd;
+    void   *buf;
+    size_t  len;
+    int     flags;
+    off_t   offset;
 
     size_t  nbytes_left;
     void   *saved_buf;
@@ -346,7 +346,7 @@ static inline void urev_op_set_err_code(urev_op_common_t *common, int err_code)
  * with calling urev_submit and urev_wait_and_handle_completion repeatedly.
  *
  * @param [in,out] queue a queue.
- * @param [in,out] op    an accept operatoin.
+ * @param [in,out] op    an accept operation.
  * @return zero if success, -errno from urev_submit or urev_wait_and_handle_completion if error.
  */
 int urev_prep_accept(urev_queue_t *queue, urev_accept_op_t *op);
@@ -364,15 +364,15 @@ int urev_prep_sendmsg(urev_queue_t *queue, urev_recvmsg_or_sendmsg_op_t *op);
 int urev_prep_statx(urev_queue_t *queue, urev_statx_op_t *op);
 int urev_prep_timeout(urev_queue_t *queue, urev_timeout_op_t *op);
 /**
- * Prepare a timeout_cancel operation.
+ * Get a sqe and prepare a timeout_cancel operation.
  *
  * If the submission queue is full, this function tries to make room
  * with calling urev_submit and urev_wait_and_handle_completion repeatedly.
  *
- * @param [in,out] sqe  a submission queue entry.
- * @param [in,out] op   a timeout_cancel operation.
- *                      op->target_op must be set to the target
- *                      timeout operation.
+ * @param [in,out] queue a queue.
+ * @param [in,out] op    a timeout_cancel operation.
+ *                       op->target_op must be set to the target
+ *                       timeout operation.
  * @return zero if success, -errno from urev_submit or urev_wait_and_handle_completion if error.
  */
 int urev_prep_timeout_cancel(urev_queue_t *queue, urev_timeout_cancel_op_t *op);
