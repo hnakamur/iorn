4f6bf7dc8f738912c54a4b82c670cebb5305db05
Hiroaki Nakamura <hnakamur@gmail.com>
2020-05-16 20:29:12 +0000

    Add functions for adjusting and restoring iovecs after short read/writes

--- src/include/iorn/iovecs.h
+++ src/include/iorn/iovecs.h
@@ -20,6 +20,9 @@ static inline void iorn_iovec_init(iorn_iovec_t *vec)
     memset(vec, 0, sizeof(*vec));
 }
 
+void iorn_iovec_adjust_after_short(iorn_iovec_t *vec, size_t advance, void **saved_iov_base);
+void iorn_iovec_restore_from_short_adjust(iorn_iovec_t *vec, void **saved_iov_base);
+
 static inline void iorn_iovec_free_ma(iorn_iovec_t *vec, iorn_malloc_t *ma)
 {
     ma->free(vec->iov_base, ma->user_data);
@@ -28,13 +31,20 @@ static inline void iorn_iovec_free_ma(iorn_iovec_t *vec, iorn_malloc_t *ma)
 /* iorn_iovecs_t */
 
 typedef struct iorn_iovecs iorn_iovecs_t;
+typedef struct iorn_iovecs_save_for_short iorn_iovecs_save_for_short_t;
 
 struct iorn_iovecs {
     size_t        nmemb;
     iorn_iovec_t *vecs;
 };
 
+struct iorn_iovecs_save_for_short {
+    iorn_iovec_t *vecs;
+    void         *iov_base;
+};
+
 #define IORN_IOVECS_EMPTY { .nmemb = 0, .vecs = NULL }
+#define IORN_IOVECS_SAVE_FOR_SHORT_EMPTY { .vecs = NULL, iov_base = NULL }
 
 static inline void iorn_iovecs_init(iorn_iovecs_t *vecs)
 {
@@ -43,6 +53,9 @@ static inline void iorn_iovecs_init(iorn_iovecs_t *vecs)
 
 iorn_negative_errno_t iorn_iovecs_add_vec(iorn_iovecs_t *vecs, iorn_iovec_t *vec, iorn_malloc_t *ma);
 iorn_negative_errno_t iorn_iovecs_resize(iorn_iovecs_t *vecs, size_t nmemb, iorn_malloc_t *ma);
+
+void iorn_iovecs_adjust_after_short(iorn_iovecs_t *vecs, size_t advance, iorn_iovecs_save_for_short_t *save);
+void iorn_iovecs_restore_from_short_adjust(iorn_iovecs_t *vecs, iorn_iovecs_save_for_short_t *save);
 
 static inline void iorn_iovecs_shallow_free_ma(iorn_iovecs_t *vecs, iorn_malloc_t *ma)
 {
--- src/iorn_iovecs.c
+++ src/iorn_iovecs.c
@@ -1,6 +1,60 @@
 #include "iorn/error.h"
 #include "iorn/iovecs.h"
 
+/* functions for iorn_iovec_t */
+
+void iorn_iovec_adjust_after_short(iorn_iovec_t *vec, size_t advance, void **saved_iov_base)
+{
+    if (*saved_iov_base == NULL) {
+        *saved_iov_base = vec->iov_base;
+    }
+    vec->iov_len -= advance;
+    vec->iov_base += advance;
+}
+
+void iorn_iovec_restore_from_short_adjust(iorn_iovec_t *vec, void **saved_iov_base)
+{
+    if (*saved_iov_base != NULL) {
+        vec->iov_len += (const char *) vec->iov_base - (const char *) saved_iov_base;
+        vec->iov_base = *saved_iov_base;
+        *saved_iov_base = NULL;
+    }
+}
+
+/* functions for iorn_iovecs_t */
+
+void iorn_iovecs_adjust_after_short(iorn_iovecs_t *vecs, size_t advance, iorn_iovecs_save_for_short_t *save)
+{
+    if (save->vecs == NULL) {
+        save->vecs = vecs->vecs;
+    }
+    iorn_iovec_t *vec = vecs->vecs;
+    if (advance >= vec->iov_len) {
+        iorn_iovec_restore_from_short_adjust(vec, &save->iov_base);
+        advance -= vec->iov_len;
+        vecs->nmemb--;
+        vec++;
+    }
+    for (; advance >= vec->iov_len; advance -= vec->iov_len) {
+        vecs->nmemb--;
+        vec++;
+    }
+    if (advance > 0) {
+        iorn_iovec_adjust_after_short(vec, advance, &save->iov_base);
+    }
+    vecs->vecs = vec;
+}
+
+void iorn_iovecs_restore_from_short_adjust(iorn_iovecs_t *vecs, iorn_iovecs_save_for_short_t *save)
+{
+    if (save->vecs != NULL) {
+        iorn_iovec_restore_from_short_adjust(vecs->vecs, &save->iov_base);
+        vecs->nmemb += vecs->vecs - save->vecs;
+        vecs->vecs = save->vecs;
+        save->vecs = NULL;
+    }
+}
+
 iorn_negative_errno_t iorn_iovecs_resize(iorn_iovecs_t *vecs, size_t nmemb, iorn_malloc_t *ma)
 {
     iorn_iovec_t *new_vecs = ma->reallocarray(vecs->vecs, nmemb, sizeof(iorn_iovec_t), ma->user_data);
