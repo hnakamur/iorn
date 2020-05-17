4d3e8179ed1067997182fbecce93df63ff3a0d97
Hiroaki Nakamura <hnakamur@gmail.com>
2020-05-17 02:13:14 +0000

    Delete debug prints

--- src/iorn_iovec_array.c
+++ src/iorn_iovec_array.c
@@ -7,24 +7,20 @@
 
 size_t iorn_iovec_adjust_after_short(size_t iov_len, void **iov_base, size_t advance, void **save_iov_base)
 {
-    fprintf(stderr, "iorn_iovec_adjust_after_short start, iov_len=%ld, *iov_base=%p, advance=%ld, *save_iov_base=%p\n", iov_len, *iov_base, advance, *save_iov_base);
     if (*save_iov_base == NULL) {
         *save_iov_base = *iov_base;
     }
     *iov_base = (char *) (*iov_base) + advance;
-    fprintf(stderr, "iorn_iovec_adjust_after_short exit, iov_len=%ld, *iov_base=%p, *save_iov_base=%p\n", iov_len - advance, *iov_base, *save_iov_base);
     return iov_len - advance;
 }
 
 size_t iorn_iovec_restore_from_short_adjust(size_t adjusted_iov_len, void **adjusted_iov_base, void **save_iov_base)
 {
-    fprintf(stderr, "iorn_iovec_restore_from_short_adjust start, iov_len=%ld, *iov_base=%p, *save_iov_base=%p\n", adjusted_iov_len, *adjusted_iov_base, *save_iov_base);
     if (*save_iov_base != NULL) {
         adjusted_iov_len += (char *) *adjusted_iov_base - (char *) *save_iov_base;
         *adjusted_iov_base = *save_iov_base;
         *save_iov_base = NULL;
     }
-    fprintf(stderr, "iorn_iovec_restore_from_short_adjust exit, iov_len=%ld, *iov_base=%p, *save_iov_base=%p\n", adjusted_iov_len, *adjusted_iov_base, *save_iov_base);
     return adjusted_iov_len;
 }
 
@@ -32,7 +28,6 @@ size_t iorn_iovec_restore_from_short_adjust(size_t adjusted_iov_len, void **adju
 
 size_t iorn_iovec_array_adjust_after_short(size_t vecs_len, iorn_iovec_t **vecs, size_t advance, iorn_iovec_t **save_vecs, void **save_iov_base)
 {
-    fprintf(stderr, "iorn_iovec_array_adjust_after_short start, vecs_len=%ld, *vecs=%p, advance=%ld\n", vecs_len, *vecs, advance);
     if (*save_vecs == NULL) {
         *save_vecs = *vecs;
     }
@@ -42,18 +37,15 @@ size_t iorn_iovec_array_adjust_after_short(size_t vecs_len, iorn_iovec_t **vecs,
         vec->iov_len = iorn_iovec_restore_from_short_adjust(vec->iov_len, &vec->iov_base, save_iov_base);
         vecs_len--;
         vec++;
-        fprintf(stderr, "iorn_iovec_array_adjust_after_short after first vec, vecs_len=%ld, vec=%p, advance=%ld\n", vecs_len, vec, advance);
     }
     for (; advance >= vec->iov_len; advance -= vec->iov_len) {
         vecs_len--;
         vec++;
     }
-    fprintf(stderr, "iorn_iovec_array_adjust_after_short after skipping full vec, vecs_len=%ld, vec=%p, advance=%ld\n", vecs_len, vec, advance);
     if (advance > 0) {
         vec->iov_len = iorn_iovec_adjust_after_short(vec->iov_len, &vec->iov_base, advance, save_iov_base);
     }
     *vecs = vec;
-    fprintf(stderr, "iorn_iovec_array_adjust_after_short exit, vecs_len=%ld, vec=%p\n", vecs_len, vec);
     return vecs_len;
 }
 
--- test/iovec_array_adjust_after_short.c
+++ test/iovec_array_adjust_after_short.c
@@ -12,15 +12,15 @@ static size_t copy_to_iovecs(unsigned nr_vecs, struct iovec *vecs, void *src, si
     size_t n;
 
     copied = 0;
-    fprintf(stderr, "copy_to_iovecs start, nr_vecs=%d, nbytes=%ld\n", nr_vecs, nbytes);
+    //fprintf(stderr, "copy_to_iovecs start, nr_vecs=%d, nbytes=%ld\n", nr_vecs, nbytes);
     while (nbytes > 0 && nr_vecs > 0) {
         n = nbytes;
         if (n > vecs[0].iov_len) {
             n = vecs[0].iov_len;
         }
-        fprintf(stderr, "before memcpy to %p, n=%ld\n", vecs[0].iov_base, n);
+        //fprintf(stderr, "before memcpy to %p, n=%ld\n", vecs[0].iov_base, n);
         memcpy(vecs[0].iov_base, src, n);
-        fprintf(stderr, "memcpy to %p, n=%ld\n", vecs[0].iov_base, n);
+        //fprintf(stderr, "memcpy to %p, n=%ld\n", vecs[0].iov_base, n);
         nbytes -= n;
         copied += n;
         src += n;
@@ -28,7 +28,7 @@ static size_t copy_to_iovecs(unsigned nr_vecs, struct iovec *vecs, void *src, si
             nr_vecs--;
             vecs++;
         }
-        fprintf(stderr, "loop continues, nr_vecs=%d, nbytes=%ld\n", nr_vecs, nbytes);
+        //fprintf(stderr, "loop continues, nr_vecs=%d, nbytes=%ld\n", nr_vecs, nbytes);
     }
     return copied;
 }
@@ -46,19 +46,19 @@ static int test_iorn_iovec_adjust_after_short(void)
         }
         src_buf[i * BUF_LEN + BUF_LEN - 1] = i != NR_VECS - 1 ? '\n' : '\0';
     }
-    fprintf(stderr, "%s\n", src_buf);
-    fprintf(stderr, "---\n");
+    //fprintf(stderr, "%s\n", src_buf);
+    //fprintf(stderr, "---\n");
 
     memset(dst_buf, 0, sizeof(dst_buf));
     memset(&h, 0, sizeof(h));
     h.msg_iovlen = NR_VECS;
     h.msg_iov = vecs;
-    fprintf(stderr, "vecs=%p\n", vecs);
-    fprintf(stderr, "vecs + 1=%p\n", vecs + 1);
+    //fprintf(stderr, "vecs=%p\n", vecs);
+    //fprintf(stderr, "vecs + 1=%p\n", vecs + 1);
     for (int i = 0; i < NR_VECS; i++) {
         vecs[i].iov_len = BUF_LEN;
         vecs[i].iov_base = dst_buf + BUF_LEN * i;
-        fprintf(stderr, "i=%d, iov_len=%ld, iov_base=%p\n", i, vecs[i].iov_len, vecs[i].iov_base);
+        //fprintf(stderr, "i=%d, iov_len=%ld, iov_base=%p\n", i, vecs[i].iov_len, vecs[i].iov_base);
     }
 
     iorn_iovec_t *save_vecs = NULL;
@@ -70,19 +70,19 @@ static int test_iorn_iovec_adjust_after_short(void)
         size_t copied = copy_to_iovecs(h.msg_iovlen, h.msg_iov, src_buf + total_copied, advance);
         total_copied += advance;
         if (copied != advance) {
-            fprintf(stderr, "unexpected copied=%ld, advance=%ld\n", copied, advance);
+            //fprintf(stderr, "unexpected copied=%ld, advance=%ld\n", copied, advance);
         }
-        //fprintf(stderr, "after copy i=%d, dst_buf=[%s]\n", i, dst_buf);
-        fprintf(stderr, "before adjust i=%d\n", i);
+        ////fprintf(stderr, "after copy i=%d, dst_buf=[%s]\n", i, dst_buf);
+        //fprintf(stderr, "before adjust i=%d\n", i);
         h.msg_iovlen = iorn_iovec_array_adjust_after_short(h.msg_iovlen, &h.msg_iov, advance, &save_vecs, &save_iov_base);
-        fprintf(stderr, "after adjust i=%d, iovlen=%ld, iov=%p\n", i, h.msg_iovlen, h.msg_iov);
+        //fprintf(stderr, "after adjust i=%d, iovlen=%ld, iov=%p\n", i, h.msg_iovlen, h.msg_iov);
     }
-    fprintf(stderr, "before restore\n");
+    //fprintf(stderr, "before restore\n");
     h.msg_iovlen = iorn_iovec_array_restore_from_short_adjust(h.msg_iovlen, &h.msg_iov, &save_vecs, &save_iov_base);
-    fprintf(stderr, "after restore\n");
+    //fprintf(stderr, "after restore\n");
 
     for (int i = 0; i < NR_VECS; i++) {
-        fprintf(stderr, "before memcmp i=%d\n", i);
+        //fprintf(stderr, "before memcmp i=%d\n", i);
         if (memcmp(src_buf + BUF_LEN * i, h.msg_iov[i].iov_base, h.msg_iov[i].iov_len) != 0) {
             fprintf(stderr, "unmatched bytes in vec %i, [%.*s], [%.*s]\n",
                     i, BUF_LEN, src_buf + BUF_LEN * i,
